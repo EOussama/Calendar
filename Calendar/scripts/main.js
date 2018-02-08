5aa87cccf7bf0e70be07a8730db25be294751b87
Oussama <Oussama_gtacj@live.fr>
2018-02-08 00:19:18 +0000

    Add files via upload
    Bug fixes.

--- Calendar/scripts/main.js
+++ Calendar/scripts/main.js
@@ -1,15 +1,27 @@
-var monthsName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var monthElement = document.getElementById('monthName');
-var timeElement = document.getElementById('time');
-var timer;
-var date = new Date(), absDate = new Date();
-var currentIter = 0;
-var table = document.getElementsByTagName('table')[0];
-var rows = table.getElementsByTagName('tr');
-var cells;
+var 
+	timer,
+	curSet = false,
+	currentIter = 0,
+	date = new Date(),
+	absDate = new Date();
+	monthElement = document.getElementById('monthName'),
+	monthsName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var
+	timeElement = document.getElementById('time'),
+	table = document.getElementsByTagName('table')[0],
+	rows = table.getElementsByTagName('tr'),
+	cells;
 
 document.body.onload = function() {
-	startCount();
+	timer = setInterval(function() {
+		
+		absDate = new Date();
+		updateTime();
+		
+	}, 1000);
+	
+	updateCal(date);
 	console.info('Calendar started - ' + new Date());
 }
 
@@ -30,21 +42,6 @@ function updateTime() {
 	timeElement.innerHTML = absDate.getHours() + ":" + (absDate.getMinutes() < 10 ? '0' : '') + absDate.getMinutes() + ":" + (absDate.getSeconds() < 10 ? '0' : '') + absDate.getSeconds();
 }
 
-function startCount() {
-	
-	setInterval(function() {
-		
-		if(date.getMonth() == absDate.getMonth())
-		{
-			let x = 0;
-			absDate = new Date();
-
-			updateTime();
-			updateCal(date);
-		}
-	}, 1000);
-}
-
 function updateCal(dt) {
 	
 	let x = 0;
@@ -58,44 +55,44 @@ function updateCal(dt) {
 		for(let j = 0; j<cells.length; j++)
 		{
 			cells[j].innerHTML = '';
+			cells[j].classList.remove('oor');
 
-			if((new Date(dt.getFullYear(), dt.getMonth() + 1, 0)).getDate() <= x)
-				continue;
-
-			if((new Date(dt.getFullYear(), dt.getMonth(), 0)).getDay() < j)
-				cells[j].innerHTML = ++x;
-			else if(i != 1)
-				cells[j].innerHTML = ++x;
-
-			if(cells[j].innerText == dt.getDate() && dt.getMonth() == absDate.getMonth() && dt.getFullYear() == absDate.getFullYear())
-				cells[j].classList.add('currentDay');
-			else
-				cells[j].classList.remove('currentDay');
-		}
-	}
-	
-	fillOutOfRangeDays(dt);
-}
-
-function fillOutOfRangeDays(dt) {
-	
-	let x = 0;
-	
-	for(let i = 0; i<rows.length; i++)
-	{
-		cells = rows[i].getElementsByTagName('td');
-
-		for(let j = 0; j<cells.length; j++)
-		{
-			if(cells[j].innerHTML == '')
+			if(i == 1)
 			{
-				cells[j].innerHTML = x;
-				cells[j].classList.add('oor');
+				if(j >= dt.getDay())
+					cells[j].innerHTML = ++x;
+				
+				else
+				{
+					cells[j].innerHTML = (new Date(dt.getFullYear(), dt.getMonth(), (new Date(dt.getFullYear(), dt.getMonth(), 0)).getDate() - (new Date(dt.getFullYear(), dt.getMonth(), 1)).getDay() - j)).getDate();
+					cells[j].classList.add('oor');
+				}
+			}
+			
+			else if(i >= rows.length - 2)
+			{
+				if(x < (new Date(dt.getFullYear(), dt.getMonth() + 1, 0)).getDate() && x > 20)
+					cells[j].innerHTML = ++x;
+				else
+				{
+					if(x == (new Date(dt.getFullYear(), dt.getMonth() + 1, 0)).getDate())
+						x = 0;
+					
+					cells[j].innerHTML = (new Date(dt.getFullYear(), dt.getMonth() + 1, ++x)).getDate();
+					cells[j].classList.add('oor');
+				}
+			}
+			
+			else
+				cells[j].innerHTML = ++x;
+
+			if(cells[j].innerText == dt.getDate() && dt.getMonth() == absDate.getMonth() && dt.getFullYear() == absDate.getFullYear() && curSet == false)
+			{
+				cells[j].classList.add('currentDay');
+				curSet = true;
 			}
 			else
-				cells[j].classList.remove('oor');
-			
-			x++;
+				cells[j].classList.remove('currentDay');
 		}
 	}
 }
