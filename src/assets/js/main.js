c12dc89e00c9b7a89dced3c1d90bd61aeb3183ac
Eoussama <oussama_gtacj@live.fr>
2018-12-04 11:55:39 +0000

    Filled the calendar.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,44 +19,57 @@ window.addEventListener('load', () => {
         settingsBtn = document.getElementById('settings-btn'),
 		closeBtn = document.getElementById('close-btn');
 	
-	let currentDate = new Date();
+	let curDate = new Date();
 
-    // Generating the calendar's rows.
+    // #region Generating the calendar's rows.
     (() => {
-        for (let i in [...Array(5)]) {
-            const row = document.createElement('tr');
+        for (let i in [...Array(6)]) {
+			const
+				dayOfWeekOfFirstDayOfMonth = (new Date(curDate.getFullYear(), curDate.getMonth(), 1)).getDay(),
+				dayOfWeekOfLastDayOfMonth = (new Date(curDate.getFullYear(), curDate.getMonth() + 1, 1)).getDay(),
+				lastDayOfCurrentMonth = (new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0)).getDate(),
+				lastDayOfLastMonth = (new Date(curDate.getFullYear(), curDate.getMonth(), 0)).getDate(),
+				row = document.createElement('tr');
 
             for (let j in [...Array(7)]) {
-                const column = document.createElement('td');
+				const column = document.createElement('td');
+				let dayOnCalendar = ((parseInt(j) + 1) + (parseInt(i) * 7)) - dayOfWeekOfFirstDayOfMonth;
 
                 // Affecting text to the column.
-                column.textContent = (parseInt(j) + 1) + (parseInt(i) * 7);
+				if (dayOnCalendar <= 0) {
+					dayOnCalendar = lastDayOfLastMonth - ((dayOfWeekOfFirstDayOfMonth * (i + 1)) - j - 1);
+				} else if (dayOnCalendar > lastDayOfCurrentMonth) {
+					dayOnCalendar = j - dayOfWeekOfLastDayOfMonth + 1;
+				}
+
+				column.textContent = Math.abs(dayOnCalendar);
 
                 // Appending the column.
                 row.appendChild(column);
             }
             
             // Adding the new row.
-            rowContainer.appendChild(row);
+			rowContainer.appendChild(row);
 		}
-		
-		console.log(currentDate.getMonth());
 	})();
+	// #endregion
 
+	// #region Real-time for the caption.
 	setInterval(() => {
-		currentDate = new Date();
+		curDate = new Date();
 
 		// Updating the month.
-		monthYearLabel.textContent = `${ monthsName[currentDate.getMonth()] } ${ currentDate.getFullYear() }`;
+		monthYearLabel.textContent = `${ monthsName[curDate.getMonth()] } ${ curDate.getFullYear() }`;
 
 		// Updating the time.
 		const
-			_hours = (currentDate.getHours() < 10 ? `0${ currentDate.getHours() }` : currentDate.getHours()),
-			_minutes = (currentDate.getMinutes() < 10 ? `0${ currentDate.getMinutes() }` : currentDate.getMinutes()),
-			_seconds = (currentDate.getSeconds() < 10 ? `0${ currentDate.getSeconds() }` : currentDate.getSeconds());
+			_hours = (curDate.getHours() < 10 ? `0${ curDate.getHours() }` : curDate.getHours()),
+			_minutes = (curDate.getMinutes() < 10 ? `0${ curDate.getMinutes() }` : curDate.getMinutes()),
+			_seconds = (curDate.getSeconds() < 10 ? `0${ curDate.getSeconds() }` : curDate.getSeconds());
 
 		timeLabel.textContent = `${ _hours } : ${ _minutes } : ${ _seconds }`;
 	}, 1000);
+	// #endregion
 
     /**
      * The click event that opens the modal.
